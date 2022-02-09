1823a5749ca6eaf0f37b5f93ad5a60ed0bda7ffa
jennyxchang <81837780+jennyxchang@users.noreply.github.com>
2022-02-09 02:09:43 -0600

    add UI, dynamic dropdown, and search function

---
+++ main.py
@@ -0,0 +1,69 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QTextBrowser, QComboBox, QListWidget
+from PyQt5 import uic
+import sys
+import sqlite3
+
+class UI(QMainWindow):
+    def __init__(self):
+        super(UI, self).__init__()
+
+        uic.loadUi("mainWindow.ui", self)
+        self.setWindowTitle("Airport Information Generator")
+
+        self.airportSelect = self.findChild(QComboBox, "airportSelect")
+        self.searchButton = self.findChild(QPushButton, "searchButton")
+        self.lastResultButton = self.findChild(QPushButton, "lastResultButton")
+        self.airportCodeLabel = self.findChild(QLabel, "airportCodeLabel")
+        self.airportNameLabel = self.findChild(QLabel, "airportNameLabel")
+        self.locationLabel = self.findChild(QLabel, "locationLabel")
+        self.covidLabel = self.findChild(QLabel, "covidLabel")
+        self.nearbyButton = self.findChild(QPushButton, "nearbyButton")
+        self.nearbyList = self.findChild(QListWidget, "nearbyList")
+
+        self.isFirstSearch = True
+        self.airportCodeLabel.hide()
+        self.airportNameLabel.hide()
+        self.locationLabel.hide()
+        self.covidLabel.hide()
+        self.nearbyButton.hide()
+        self.nearbyList.hide()
+
+        conn = sqlite3.connect("airports.db")
+        cur = conn.cursor()
+        cur.execute("SELECT airportCode FROM Airports;")
+        selectData = cur.fetchall()
+        conn.close()
+
+        for row in selectData:
+            self.airportSelect.addItem(row[0])
+
+        self.searchButton.clicked.connect(self.search)
+        self.show()
+
+    def search(self):
+        airportCode = self.airportSelect.currentText()
+        conn = sqlite3.connect("airports.db")
+        cur = conn.cursor()
+        cur.execute("SELECT airportName, airportCity, airportState, airportZipcode, covidTesting FROM Airports WHERE airportCode = ?;", (airportCode,))
+        airportName, airportCity, airportState, airportZipcode, covidTesting = cur.fetchall()[0]
+        conn.close()
+
+        self.airportCodeLabel.setText(f'Airport Code: {airportCode}')
+        self.airportNameLabel.setText(f'Airport Name: {airportName}')
+        self.locationLabel.setText(f'Location: {airportCity}, {airportState} {airportZipcode}')
+        if covidTesting == 0:
+            self.covidLabel.setText("COVID Testing: No")
+        else:
+            self.covidLabel.setText("COVID Testing: Yes")
+
+        if (self.isFirstSearch == True):
+            self.airportCodeLabel.show()
+            self.airportNameLabel.show()
+            self.locationLabel.show()
+            self.covidLabel.show()
+            self.nearbyButton.show()
+            self.isFirstSearch = False
+
+app = QApplication(sys.argv)
+UIWindow = UI()
+app.exec_()
