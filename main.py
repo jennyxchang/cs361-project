49e74bf7106a58cec82a68a3d1521f6de3013143
jennyxchang <81837780+jennyxchang@users.noreply.github.com>
2022-02-18 17:25:37 -0600

    add function to request teammate's microservice - place finder

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QCom
 from PyQt5 import uic
 import sys
 import sqlite3
+import os
+import csv
+
 
 class UI(QMainWindow):
     def __init__(self):
@@ -30,6 +33,7 @@ class UI(QMainWindow):
         self.nearbyList.hide()
 
         self.resultHistory = []
+        self.requestZipcode = ""
 
         conn = sqlite3.connect("airports.db")
         cur = conn.cursor()
@@ -41,28 +45,30 @@ class UI(QMainWindow):
             self.airportSelect.addItem(row[0])
 
         self.searchButton.clicked.connect(self.search)
-        #self.prevResultButton.clicked.connect(self.prevResult)
         self.prevResultButton.clicked.connect(self.alertBox)
         self.nearbyButton.clicked.connect(self.nearbySearch)
 
         self.show()
 
-
     def loadAirportData(self, airportCode):
         conn = sqlite3.connect("airports.db")
         cur = conn.cursor()
-        cur.execute("SELECT airportName, airportCity, airportState, airportZipcode, covidTesting FROM Airports WHERE airportCode = ?;", (airportCode,))
-        airportName, airportCity, airportState, airportZipcode, covidTesting = cur.fetchall()[0]
+        cur.execute(
+            "SELECT airportName, airportCity, airportState, airportZipcode, covidTesting FROM Airports WHERE airportCode = ?;", (airportCode,))
+        airportName, airportCity, airportState, airportZipcode, covidTesting = cur.fetchall()[
+            0]
         conn.close()
 
         self.airportCodeLabel.setText(f'Airport Code: {airportCode}')
         self.airportNameLabel.setText(f'Airport Name: {airportName}')
-        self.locationLabel.setText(f'Location: {airportCity}, {airportState} {airportZipcode}')
+        self.locationLabel.setText(
+            f'Location: {airportCity}, {airportState} {airportZipcode}')
         if covidTesting == 0:
             self.covidLabel.setText("COVID Testing: No")
         else:
             self.covidLabel.setText("COVID Testing: Yes")
 
+        self.requestZipcode = airportZipcode
 
     def search(self):
         airportCode = self.airportSelect.currentText()
@@ -84,31 +90,56 @@ class UI(QMainWindow):
         self.nearbyList.clear()
         self.nearbyList.hide()
 
-
     def alertBox(self):
         alertMessage = QMessageBox()
         alertMessage.setWindowTitle("Show previous search result?")
-        alertMessage.setText("The current result will be cleared. Are you sure?")
+        alertMessage.setText(
+            "The current result will be cleared. Are you sure?")
         alertMessage.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
         alertButton = alertMessage.exec_()
 
         if alertButton == QMessageBox.Yes:
             self.prevResult()
 
-
     def prevResult(self):
         if len(self.resultHistory) == 2:
             self.prevResultButton.hide()
-        
+
         self.resultHistory.pop()
         airportCode = self.resultHistory[-1]
         self.loadAirportData(airportCode)
 
+        self.nearbyList.clear()
+        self.nearbyList.hide()
+
+    def requestPlaceFinder(self):
+        # Sends a request to teammate's microservice - place finder.
+        with open("./place-finder/input.txt", "w") as file:
+            file.write(
+                f'["{self.requestZipcode}", "US", 10, "5", ["restaurants"]]')
+
+        # Waits for teammate's microservice to send response.
+        while True:
+            if os.path.exists("./place-finder/status.txt"):
+                os.remove("./place-finder/status.txt")
+                break
+            else:
+                continue
+
+        restaurants = []
+        # Reads response.
+        with open('./place-finder/output.csv', 'r') as file:
+            csvFile = csv.DictReader(file)
+            for row in csvFile:
+                restaurants.append(dict(row)["name"])
+        return restaurants
 
     def nearbySearch(self):
         self.nearbyList.clear()
-        exampleList = ["Beecher's Handmade Cheese", "Bambuza Vietnam Kitchen and Bar", "Bigfoot Food & Spirits", "Seattle Seahawks 12 Club"]
-        for item in exampleList:
+        restaurants = self.requestPlaceFinder()
+        # exampleList = ["Beecher's Handmade Cheese", "Bambuza Vietnam Kitchen and Bar",
+        #               "Bigfoot Food & Spirits", "Seattle Seahawks 12 Club"]
+        for item in restaurants:
             self.nearbyList.addItem(item)
         self.nearbyList.show()
 
